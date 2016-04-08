dbd710de489e0a6ac0048e76c52a182fcc027450
piling <pirebok@protonmail.com>
2016-04-09 02:44:44 +0300

    minor change

--- test.c
+++ test.c
@@ -79,14 +79,15 @@ int main(void)
         uint64_t bencode_len_of_list;
         // Reading 64 bit length of list
         fread(&bencode_len_of_list, sizeof bencode_len_of_list, 1, stdin);
-        // swapping endiannes and returning len of string
         bencode_len_of_list = htobe64(bencode_len_of_list);
         char bencode_string[bencode_len_of_list];
         fread(&bencode_string, bencode_len_of_list, 1, stdin);
 
         putchar(1);
-        fwrite(&bencode_len_of_list, sizeof bencode_len_of_list, 1, stdout);
-        fwrite(&bencode_string, sizeof bencode_string, 1, stdout);
+        char length[] = "\x00\x00\x00\x00\x00\x00\x00\x08";
+        fwrite(&length, sizeof length-1, 1, stdout);
+        //fwrite(&bencode_len_of_list, sizeof bencode_len_of_list, 1, stdout);
+        fwrite(&bencode_string, bencode_len_of_list, 1, stdout);
     }
     else if(!memcmp(test_name, "BinaryDecode String", len_of_test_name)){
         putchar(2);
